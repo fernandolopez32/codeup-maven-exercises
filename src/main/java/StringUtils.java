5449eea6fd84c4f4a03480d371d2c8dfb71da1b5
Fernando Lopez <flopez00032@gmail.com>
2022-11-02 11:31:26 -0500

    finished exercise

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -4,11 +4,19 @@ public class StringUtils {
 
     public static org.apache.commons.lang3.StringUtils su = new org.apache.commons.lang3.StringUtils();
 
-    public static String myMethod(String userInput){
+    public static String wetherIsNumber(String userInput){
         if(!su.isNumeric(userInput)){
             return "You entered a String";
         }
         return "You entered a number";
+    }
+
+    public static String flipCase(String userInput){
+        return su.swapCase(userInput);
+    }
+
+    public static String reverse(String input){
+        return su.reverse(input);
     }
 
     public static void main(String[] args) {
@@ -17,7 +25,12 @@ public class StringUtils {
 
         System.out.println("Enter a number");
         String userInput = sc.nextLine();
-        System.out.println(myMethod(userInput));
+
+        System.out.println(wetherIsNumber(userInput));
+        System.out.println(reverse(userInput));
+        System.out.println(flipCase(userInput));
+
+
     }
 
 }
